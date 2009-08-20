d3056bb557232332ec1721010d98016546dd640f
frsyuki <frsyuki@users.sourceforge.jp>
2009-08-20 11:43:35 +0900

    add README

--- src/serve.c
+++ src/serve.c
@@ -1,3 +1,26 @@
+/*
+ * daemonutils serve
+ *
+ * Copyright (c) 2009 FURUHASHI Sadayuki
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
@@ -31,7 +54,7 @@ static int open_ctl(const char* name)
 	strcat(pathbuf, name);
 	strcat(pathbuf, "/" SERVE_CTL_FILE);
 
-	int ret = open(pathbuf, O_RDONLY | O_NDELAY);
+	int ret = open(pathbuf, O_WRONLY | O_NDELAY);
 	if(ret < 0) {
 		fprintf(stderr, "can't open %s: %s\n", pathbuf, strerror(errno));
 	}
